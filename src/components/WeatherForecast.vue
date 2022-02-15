5c56e27cad26c44c79181d472aa3d1c4cdff592b
dailcoyote <yergaliyev.production@gmail.com>
2022-02-16 05:10:24 +0600

    @new feature [abc index & search func]

--- src/components/WeatherForecast.vue
+++ src/components/WeatherForecast.vue
@@ -8,6 +8,7 @@
 
 <script>
 import { defineComponent } from "vue";
+import CityRepository from "../repositories/Cities";
 import WeatherLocation from "./WeatherLocation.vue";
 import WeatherStats from "./WeatherStats.vue";
 import WeatherBack from "./WeatherBack.vue";
@@ -17,14 +18,22 @@ export default defineComponent({
   components: {
     WeatherLocation,
     WeatherStats,
-    WeatherBack
+    WeatherBack,
+  },
+  created() {
+    CityRepository.createABCIndex();
+    console.log(CityRepository.abcIndex);
+
+    console.log('Searching...', new Date())
+    let resultSet = CityRepository.getSuggestions('Atlanta');
+    console.log('Finish...', new Date(), resultSet);
   },
 });
 </script>
 
 <style scoped>
 #weather__section {
-    margin: 21px 0;
-    width: 375px;
+  margin: 21px 0;
+  width: 375px;
 }
 </style>
---
+++ src/repositories/Cities.js
@@ -0,0 +1,51 @@
+import Storage from "../data/city.list.json";
+
+class Cities {
+    constructor() {
+        this._abcIndex = new Map();
+    }
+    createABCIndex() {
+        console.log('Building...', new Date())
+        for (let index = 0; index < Storage.length; index++) {
+            const cursor = Storage[index];
+            const capital = cursor.name.charAt(0);
+
+            if (!this._abcIndex.has(capital)) {
+                this._abcIndex.set(capital, [cursor]);
+                continue;
+            }
+
+            const branch = this._abcIndex.get(capital);
+            branch.push(cursor);
+            this._abcIndex.set(capital, branch);
+        }
+        console.log('Finish...', new Date())
+    }
+    get abcIndex() {
+        return this._abcIndex;
+    }
+    getSuggestions(searchTerm) {
+        const branch = this._abcIndex.get(searchTerm.charAt(0)) || [];
+        let matches = 0;
+        let suggestions = [];
+
+        for (let index = 0; index < branch.length; index++) {
+            const cursor = branch[index];
+            if (
+                cursor.name.toLowerCase().includes(searchTerm.toLowerCase())
+                && matches < 10
+            ) {
+                matches++
+                suggestions.push(cursor);
+            }
+
+            if(matches > 10) {
+                break;
+            }
+        }
+
+        return suggestions;
+    }
+}
+
+export default new Cities();
